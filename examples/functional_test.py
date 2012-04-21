f6bd822bda6ba9d9c0fcde94ea65f71eade1b6d1
Federico Ceratto <federico.ceratto@gmail.com>
2012-04-21 20:26:08 +0100

    functional testing improved

--- examples/functional_test.py
+++ examples/functional_test.py
@@ -8,23 +8,47 @@
 #
 
 from nose.tools import assert_raises, raises, with_setup
-
+from time import time
 from webtest import TestApp
+import glob
+import os
+import shutil
+
 import simple_webapp
 
 REDIR = '302 Found'
 app = None
+tmpdir = None
 
 def setup_app():
+    # create test dir and populate it using the example files
+    global tmpdir
+    tstamp = str(time())[5:]
+    tmpdir = "/dev/shm/cork_functional_test_%s" % tstamp
+    os.mkdir(tmpdir)
+    os.mkdir(tmpdir + '/example_conf')
+    # copy the needed files
+    globs = ['example_conf/*.json']
+    for g in globs:
+        for f in glob.glob(g):
+            shutil.copy(f, tmpdir)
+
+    # create global TestApp instance
     global app
     app = TestApp(simple_webapp.app)
 
 def login():
+    """run setup_app and log in"""
     global app
-    app = TestApp(simple_webapp.app)
+    setup_app()
     p = app.post('/login', {'user': 'admin', 'pwd': 'admin'})
 
 def teardown():
+    global tmpdir
+    if tmpdir is not None:
+        assert tmpdir.startswith('/dev/shm/cork_functional_test_')
+        shutil.rmtree(tmpdir)
+        tmpdir = None
     app = None
 
 
