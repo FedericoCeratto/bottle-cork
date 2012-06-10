af9208fca1cd258f3d29cc9928a96f77939e0aa4
Federico Ceratto <federico.ceratto@gmail.com>
2012-06-10 16:50:00 +0100

    License updated: LGPLv3+ - release 0.1~beta3 ready

--- cork.py
+++ cork.py
@@ -3,18 +3,18 @@
 # Cork - Authentication module for the Bottle web framework
 # Copyright (C) 2012 Federico Ceratto
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This package is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #
 # Cork is designed for web application with a small userbase. User credentials
@@ -48,7 +48,7 @@ try:
 except ImportError:  # pragma: no cover
     import simplejson as json
 
-__version__ = '0.1~beta2'
+__version__ = '0.1~beta3'
 
 log = getLogger(__name__)
 
