60557b588327733ae34ec01b5c9bcaafa73ddb83
Federico Ceratto <federico.ceratto@gmail.com>
2012-09-29 16:08:27 +0100

    Docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ import time
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
-import cork.cork as my_module
+sys.path.insert(0, os.path.abspath('../cork'))
+import cork as my_module
 
 # -- General configuration -----------------------------------------------------
 
