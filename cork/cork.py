c2de6bb9a9f6e91638fc88f7e340f98a781bc554
23min <peter@23min.com>
2012-10-01 23:56:37 +0200

    Added support for MonboDb store.

--- cork/cork.py
+++ cork/cork.py
@@ -78,7 +78,13 @@ class JsonBackend(object):
         :param initialize: create empty JSON files (defaults to False)
         :type initialize: bool.
         """
-        assert directory, "Directory name must be valid"
+
+        # load default backend params
+        if not directory:
+            self._directory = os.path.abspath("example_conf")
+        self._backend_kind='JsonBackend'
+
+        #assert directory, "Directory name must be valid"
         self._directory = directory
         self.users = {}
         self._users_fname = users_fname
@@ -151,11 +157,246 @@ class JsonBackend(object):
         self._savejson('users', self.users)
 
 
+import pymongo
+import bson.json_util as json_util
+
+class MongoException(Exception):
+    """Generic Exception from MongoDbBackend"""
+    pass
+
+class MongoDbBackend(object):
+
+    def __init__(self, server, port, database, users_store='users',
+                 roles_store='roles', pending_regs_store='register', initialize=False, safe=False):
+        """Data storage class. Handles JSON files
+
+        :param users_fname: users file name (without .json)
+        :type users_fname: str.
+        :param roles_fname: roless file name (without .json)
+        :type roles_fname: str.
+        :param pending_reg_fnames: pending registrations file name (without .json)
+        :type pending_reg_fname: str.
+        :param initialize: create empty JSON files (defaults to False)
+        :type initialize: bool.
+        """
+        assert database, "Database must be valid"
+        self._database = database
+
+        self._connection = pymongo.Connection("localhost",27017)
+        self._database = self._connection[database]
+
+        self._users_collection = self._database[users_store]
+        self.users = self.Users(self._users_collection)
+
+        self._roles_collection = self._database[roles_store]
+        self.roles = self.Roles(self._roles_collection)
+
+        self._pending_reg_collection = self._database[pending_regs_store]
+        self.pending_registrations = self.PendingRegistrations(self._pending_reg_collection)
+
+        if initialize:
+            self._initialize_storage()
+
+        # dummy so we have same interface as JsonBackend
+        self._roles_fname = None
+        self._users_fname = None
+        self._pending_reg_fname = None
+
+    def _initialize_storage(self):
+        """Create empty database"""
+        # TODO: you can also clear the collections, maintain indices
+        self._users_collection.drop()
+        self._roles_collection.drop()
+        self._pending_reg_collection.drop()
+
+    # dummy
+    def _savejson(self, *arg, **kw):
+        pass
+
+    # dummy
+    def _loadjson(self, *arg, **kw):
+        pass
+
+    def _save_users(self):
+        pass
+
+    class Users(dict):
+
+        def __init__(self, users_collection=None, *args, **kwargs):
+            #assert users_collection
+            if users_collection:
+                self._users = users_collection
+            else:
+                raise MongoException("Users collection not defined")
+
+        def __getitem__(self, item):
+            user = self._users.find_one({"username":item})
+            return user
+
+        def __setitem__(self, key, value):
+            self._users.update({"username":key}, value, safe=True, upsert=True)
+
+        def __contains__(self, item):
+            r = self._users.find_one({"username":item})
+            return r is not None
+
+        def __delitem__(self, key):
+            self._users.remove({"username": key}, safe=True)
+
+        def __iter__(self):
+            all_users = self._users.find()
+            for u in all_users:
+                yield u
+
+        def __len__(self):
+            return self._users.find().count()
+
+        def pop(self, item, default=None):
+            u = self._users.find_one({"username":item})
+            if not u:
+                raise KeyError()
+            self._users.remove(u)
+            return u
+
+        def clear(self):
+            self._users.remove(safe=True)
+
+        # TODO: write test case
+        def items(self):
+            """
+            Right now this is implemented as a generator. This breaks
+            when we're accessing via index. But what if there are
+            100M users? A dictionary is not a suitable wrapper in that case anyway.
+            So we stick with the generator while Cork doesn't break this.
+            """
+            all_users = self._users.find()
+            yield [(key, self._users[key]) for key in all_users]
+
+        def keys(self):
+            all_users = self._users.find()
+            for u in all_users:
+                yield (u["username"])
+
+        def values(self):
+            all_users = self._users.find()
+            for u in all_users:
+                yield u
+
+        def iteritems(self):
+            all_users = self._users.find()
+            for doc in all_users:
+                yield doc["username"], doc
+
+        def update(self, indict):
+            self._users.update({indict.keys()[0]:indict.values()[0]},
+                {"$set":{indict.keys()[1]:indict.values()[1]}})
+
+        def get(self, item, default=None):
+            return self._users.find_one({"username":item})
+
+    class Roles(dict):
+
+        def __init__(self,roles_collection=None, *args, **kwargs):
+            if roles_collection:
+                #dict.__init__(self, *args, **kwargs)
+                self._roles = roles_collection
+                #self.update()
+            else:
+                raise MongoException("Roles collection not defined")
+
+        def __getitem__(self, item):
+            role = self._roles.find_one({"role":item})
+            return role["level"]
+
+        def __setitem__(self, key, value):
+            # allow the following syntax:
+            # roles['admin'] = 10
+            # means: {"role":"admin", "level":10}
+            doc = value
+            if isinstance(doc, int):
+                doc = {"$set": {"level":value}}
+            self._roles.update({"role":key}, doc, safe=True, upsert=True)
+
+        def __contains__(self, item):
+            r =  self._roles.find_one({"role":item})
+            return r is not None
+
+        def __delitem__(self, key):
+            self._roles.remove({"role": key}, safe=True)
+
+        def __iter__(self):
+            all_roles = self._roles.find()
+            for u in all_roles:
+                #yield u["role"]
+                yield u
+
+        def __len__(self):
+            return self._roles.find().count()
+
+        def pop(self, item, default=None):
+            r = self._roles.find_one({"role":item})
+            if not r:
+                raise KeyError()
+            self._roles.remove(r)
+            return r
+
+    class PendingRegistrations(dict):
+
+        def __init__(self,pending_registrations_collection=None, *args, **kwargs):
+            if pending_registrations_collection:
+                self._regs_coll = pending_registrations_collection
+            else:
+                raise MongoException("Registrations collection not defined")
+
+        def __getitem__(self, item):
+            pending_registration = self._regs_coll.find_one({"registration_code":item})
+            return pending_registration
+
+        def __setitem__(self, key, value):
+            self._regs_coll.update({"registration_code":key}, value, safe=True, upsert=True)
+
+        def __contains__(self, item):
+            return self._regs_coll.find_one({"registration_code":item}) is not None
+
+        def __delitem__(self, key):
+            self._regs_coll.remove({"registration_code": key}, safe=True)
+
+        def __iter__(self):
+            all_regs = self._regs_coll.find()
+            for u in all_regs:
+                yield u
+
+        def __len__(self):
+            return self._regs_coll.find().count()
+
+        def pop(self, item, default=None):
+            c = self._regs_coll.find_one({"registration_code":item})
+            if not c:
+                raise KeyError()
+            self._regs_coll.remove(c)
+            return c
+
+        def items(self):
+            all_users = self._regs_coll.find()
+            for doc in all_users:
+                yield doc["registration_code"], doc
+
+        def keys(self):
+            all_users = self._regs_coll.find()
+            for u in all_users:
+                yield [u["registration_code"]]
+
+        def get_key_helper(self, index):
+            """
+            For test_validate_registration we need a key
+            :param index: int, we discard this
+            """
+            reg = self._regs_coll.find_one()
+            return reg["registration_code"]
+
 class Cork(object):
 
-    def __init__(self, directory, email_sender=None, smtp_server=None,
-        users_fname='users', roles_fname='roles', pending_reg_fname='register',
-        initialize=False, session_domain=None):
+    def __init__(self, backend, email_sender=None, smtp_server=None, session_domain=None):
         """Auth/Authorization/Accounting class
 
         :param directory: configuration directory
@@ -165,10 +406,10 @@ class Cork(object):
         :param roles_fname: roles filename (without .json), defaults to 'roles'
         :type roles_fname: str.
         """
+        assert backend
+        self._store = backend
+
         self.mailer = Mailer(email_sender, smtp_server)
-        self._store = JsonBackend(directory, users_fname='users',
-            roles_fname='roles', pending_reg_fname='register',
-            initialize=initialize)
         self.password_reset_timeout = 3600 * 24
         self.session_domain = session_domain
 
@@ -334,7 +575,11 @@ class Cork(object):
         :returns: (role, role_level) generator (sorted by role)
         """
         for role in sorted(self._store.roles):
-            yield (role, self._store.roles[role])
+            if isinstance(role, dict):
+                # mongodb style, it stores documents, not strings
+                yield (role, self._store.roles[role["role"]])
+            else:
+                yield (role, self._store.roles[role])
 
     def create_user(self, username, role, password, email_addr=None,
         description=None):
@@ -362,6 +607,7 @@ class Cork(object):
             raise AAAException("Nonexistent user role.")
         tstamp = str(datetime.utcnow())
         self._store.users[username] = {
+            'username':username,
             'role': role,
             'hash': self._hash(username, password),
             'email_addr': email_addr,
@@ -391,8 +637,12 @@ class Cork(object):
         username)
         """
         for un in sorted(self._store.users):
-            d = self._store.users[un]
-            yield (un, d['role'], d['email_addr'], d['desc'])
+            # for mongo style backend
+            if isinstance(un, dict):
+                yield (un["username"], un["role"], un["email_addr"], un["desc"])
+            else:
+                d = self._store.users[un]
+                yield (un, d['role'], d['email_addr'], d['desc'])
 
     @property
     def current_user(self):
@@ -469,6 +719,7 @@ class Cork(object):
 
         # store pending registration
         self._store.pending_registrations[registration_code] = {
+            'registration_code': registration_code,
             'username': username,
             'role': role,
             'hash': self._hash(username, password),
@@ -495,6 +746,7 @@ class Cork(object):
         # the user data is moved from pending_registrations to _users
         username = data['username']
         self._store.users[username] = {
+            'username': data['username'],
             'role': data['role'],
             'hash': data['hash'],
             'email_addr': data['email_addr'],
@@ -664,6 +916,8 @@ class User(object):
         self.username = username
         self.role = self._cork._store.users[username]['role']
         self.level = self._cork._store.roles[self.role]
+        if isinstance(self.level, dict):
+            self.level = self.level["level"]
 
         if session is not None:
             try:
@@ -672,6 +926,10 @@ class User(object):
                 self.session_id = session['_id']
             except:
                 pass
+
+    # added to support test_get_current_user_unauth
+    def __getitem__(self, item):
+        raise AAAException
 
     def update(self, role=None, pwd=None, email_addr=None):
         """Update an user account data
@@ -690,12 +948,25 @@ class User(object):
         if role is not None:
             if role not in self._cork._store.roles:
                 raise AAAException("Nonexistent role.")
-            self._cork._store.users[username]['role'] = role
+            if isinstance(self._cork._store, JsonBackend):
+                self._cork._store.users[username]['role'] = role
+            else:
+                # mongodb
+                self._cork._store.users.update({"username": username, "role":role})
         if pwd is not None:
-            self._cork._store.users[username]['hash'] = self._cork._hash(
-                username, pwd)
+            if isinstance(self._cork._store, JsonBackend):
+                self._cork._store.users[username]['hash'] = self._cork._hash(
+                    username, pwd)
+            else:
+                self._cork._store.users.update({"username": username,
+                                                "hash":self._cork._hash(username, pwd)})
         if email_addr is not None:
-            self._cork._store.users[username]['email'] = email_addr
+            if isinstance(self._cork._store, JsonBackend):
+                self._cork._store.users[username]['email_addr'] = email_addr
+            else:
+
+                self._cork._store.users.update({"username": username,
+                                                "email_addr": email_addr})
         self._cork._store._save_users()
 
     def delete(self):
