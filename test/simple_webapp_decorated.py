1518d13e1cfa7297edbb1cdb6ef7c5e86f52a394
23min <peter@23min.com>
2012-10-03 15:10:20 +0200

    The simple webapp using decorators

---
+++ test/simple_webapp_decorated.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+#
+#
+# Cork example web application
+#
+# The following users are already available:
+#  admin/admin, demo/demo
+
+import bottle
+from beaker.middleware import SessionMiddleware
+from cork import Cork
+import logging
+
+logging.basicConfig(format='localhost - - [%(asctime)s] %(message)s', level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+# Use users.json and roles.json in the local example_conf directory
+aaa = Cork('example_conf', email_sender='federico.ceratto@gmail.com', smtp_server='mail2.eircom.net')
+# alias the authorization decorator with defaults
+authorize = aaa.make_auth_decorator(fail_redirect="/login", role="user")
+
+import datetime
+app = bottle.app()
+session_opts = {
+    'session.type': 'cookie',
+    'session.validate_key': True,
+    'session.cookie_expires': True,
+    'session.timeout': 3600 * 24, # 1 day
+    'session.encrypt_key': 'please use a random key and keep it secret!',
+    }
+app = SessionMiddleware(app, session_opts)
+
+# #  Bottle methods  # #
+
+def postd():
+    return bottle.request.forms
+
+def post_get(name, default=''):
+    return bottle.request.POST.get(name, default).strip()
+
+@bottle.post('/login')
+def login():
+    """Authenticate users"""
+    username = post_get('username')
+    password = post_get('password')
+    aaa.login(username, password, success_redirect='/', fail_redirect='/login')
+
+@bottle.route('/logout')
+def logout():
+    aaa.logout()
+
+@bottle.post('/register')
+def register():
+    """Send out registration email"""
+    aaa.register(post_get('username'), post_get('password'), post_get('email_address'))
+    return 'Please check your mailbox.'
+
+@bottle.route('/validate_registration/:registration_code')
+def validate_registration(registration_code):
+    """Validate registration, create user account"""
+    aaa.validate_registration(registration_code)
+    return 'Thanks. <a href="/login">Go to login</a>'
+
+@bottle.post('/reset_password')
+def send_password_reset_email():
+    """Send out password reset email"""
+    aaa.send_password_reset_email(
+        username=post_get('username'),
+        email_addr=post_get('email_address')
+    )
+    return 'Please check your mailbox.'
+
+@bottle.route('/change_password/:reset_code')
+@bottle.view('password_change_form')
+def change_password(reset_code):
+    """Show password change form"""
+    return dict(reset_code=reset_code)
+
+@bottle.post('/change_password')
+def change_password():
+    """Change password"""
+    aaa.reset_password(post_get('reset_code'), post_get('password'))
+    return 'Thanks. <a href="/login">Go to login</a>'
+
+
+@bottle.route('/')
+@authorize(role="king")
+def index():
+    """Only authenticated users can see this"""
+    #session = bottle.request.environ.get('beaker.session')
+    #aaa.require(fail_redirect='/login')
+    return 'Welcome! <a href="/admin">Admin page</a> <a href="/logout">Logout</a>'
+
+@bottle.route('/restricted_download')
+@authorize()
+def restricted_download():
+    """Only authenticated users can download this file"""
+    #aaa.require(fail_redirect='/login')
+    return bottle.static_file('static_file', root='.')
+
+
+
+# Admin-only pages
+
+@bottle.route('/admin')
+@authorize(role="admin", fail_redirect='/sorry_page')
+@bottle.view('admin_page')
+def admin():
+    """Only admin users can see this"""
+    #aaa.require(role='admin', fail_redirect='/sorry_page')
+    return dict(
+        current_user = aaa.current_user,
+        users = aaa.list_users(),
+        roles = aaa.list_roles()
+    )
+
+@bottle.post('/create_user')
+def create_user():
+    try:
+        aaa.create_user(postd().username, postd().role, postd().password)
+        return dict(ok=True, msg='')
+    except Exception, e:
+        return dict(ok=False, msg=e.message)
+
+@bottle.post('/delete_user')
+def delete_user():
+    try:
+        aaa.delete_user(post_get('username'))
+        return dict(ok=True, msg='')
+    except Exception, e:
+        print repr(e)
+        return dict(ok=False, msg=e.message)
+
+@bottle.post('/create_role')
+def create_role():
+    try:
+        aaa.create_role(post_get('role'), post_get('level'))
+        return dict(ok=True, msg='')
+    except Exception, e:
+        return dict(ok=False, msg=e.message)
+
+@bottle.post('/delete_role')
+def delete_role():
+    try:
+        aaa.delete_role(post_get('role'))
+        return dict(ok=True, msg='')
+    except Exception, e:
+        return dict(ok=False, msg=e.message)
+
+# Static pages
+
+@bottle.route('/login')
+@bottle.view('login_form')
+def login_form():
+    """Serve login form"""
+    return {}
+
+@bottle.route('/sorry_page')
+def sorry_page():
+    """Serve sorry page"""
+    return '<p>Sorry, you are not authorized to perform this action</p>'
+
+# #  Web application main  # #
+
+def main():
+
+    # Start the Bottle webapp
+    bottle.debug(True)
+    bottle.run(app=app, quiet=False, reloader=True)
+
+if __name__ == "__main__":
+    main()
