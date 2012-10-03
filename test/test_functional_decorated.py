a866f7175a8d8a9ecf962f93cd0cf0e4b34deaee
23min <peter@23min.com>
2012-10-03 22:47:00 +0200

    Fixes for setting up testing. Added decorator tests

--- test/test_functional_decorated.py
+++ test/test_functional_decorated.py
@@ -4,21 +4,23 @@
 # Requires WebTest http://webtest.pythonpaste.org/
 # sudo aptitude install python-webtest
 #
-# Run as: nosetests functional_test.py
+# Run as: nosetests functional_test_decorated.py
 #
 
 from nose.tools import assert_raises, raises, with_setup
 from time import time
+from datetime import datetime
 from webtest import TestApp
 import glob
-import os
+import os, sys
 import shutil
-import sys
+
+from cork import Cork
 
 REDIR = '302 Found'
 app = None
 tmpdir = None
-orig_dir = None
+orig_dir = os.getcwd()
 tmproot = None
 
 if sys.platform == 'darwin':
@@ -30,8 +32,16 @@ def populate_conf_directory():
     """Populate a directory with valid configuration files, to be run just once
     The files are not modified by each test
     """
+    global tmpdir
     tmpdir = "%s/cork_functional_test_source" % tmproot
-    cork = Cork(tmpdir, initialize=True)
+
+    # only do this once, as advertised
+    if os.path.exists(tmpdir): return
+
+    os.mkdir(tmpdir)
+    os.mkdir(tmpdir + "/example_conf")
+
+    cork = Cork(tmpdir + "/example_conf", initialize=True)
 
     cork._store.roles['admin'] = 100
     cork._store.roles['editor'] = 60
@@ -57,21 +67,21 @@ def populate_conf_directory():
     }
     cork._store._save_users()
 
+def remove_temp_dir():
+    for f in glob.glob('%s/cork_functional_test_wd*' % tmproot, ):
+        shutil.rmtree(f)
+
 def setup_app():
+
     # create test dir and populate it using the example files
     global tmpdir
     global orig_dir
 
-    # save the directory where the unit testing has been run
-    if orig_dir is None:
-        orig_dir = os.getcwd()
-    os.chdir(orig_dir)
+    # create json files to be used by Cork
+    populate_conf_directory()
 
     # purge the temporary test directory
-    if tmpdir is not None:
-        assert tmpdir.startswith('%s/cork_functional_test_' % tmproot)
-        shutil.rmtree(tmpdir)
-        tmpdir = None
+    remove_temp_dir()
 
     # populate the temporary test dir
     tstamp = str(time())[5:]
@@ -79,14 +89,19 @@ def setup_app():
     os.mkdir(tmpdir)
 
     # copy the needed files
-    shutil.copytree('test/example_conf', tmpdir + '/example_conf')
-    shutil.copytree('test/views', tmpdir + '/views')
+    tmp_source = "%s/cork_functional_test_source" % tmproot
+    shutil.copytree(tmp_source + '/example_conf', tmpdir + '/example_conf')
+    shutil.copytree(orig_dir + '/test/views', tmpdir + '/views')
+
+    # change to the temporary test directory
+    # cork relies on this being the current directory
     os.chdir(tmpdir)
 
     # create global TestApp instance
     global app
     import simple_webapp_decorated
     app = TestApp(simple_webapp_decorated.app)
+
 
 def login():
     """run setup_app and log in"""
@@ -95,12 +110,7 @@ def login():
     p = app.post('/login', {'username': 'admin', 'password': 'admin'})
 
 def teardown():
-    global tmpdir
-    os.chdir(orig_dir)
-    if tmpdir is not None:
-        assert tmpdir.startswith('%s/cork_functional_test_' % tmproot)
-        shutil.rmtree(tmpdir)
-        tmpdir = None
+    remove_temp_dir()
     app = None
 
 @with_setup(setup_app, teardown)
@@ -108,6 +118,39 @@ def test_unauthorized_page_access():
     # fetch a page successfully
     assert app.get('/admin').status == REDIR
 
+@with_setup(setup_app, teardown)
+def test_resource_protected_with_non_existing_role():
+    # fetch a page successfully
+    assert app.get('/for_kings_only', status=500, expect_errors=True).status_code == 500
+
+@with_setup(login, teardown)
+def test_resource_protected_for_user_admin_only():
+    # fetch a page successfully
+    assert app.get('/page_for_specific_user_admin').status == '200 OK'
+    # log out
+    assert app.get('/logout').status == REDIR
+    # drop the cookie
+    app.reset()
+    assert app.cookies == {}, "The cookie should be gone"
+    # fetch the same page, unsuccessfully
+    assert app.get('/page_for_specific_user_admin').status == REDIR
+
+@with_setup(setup_app(), teardown)
+def test_resource_protected_for_user_fred_only():
+
+    assert app.get('/page_for_specific_user_fred_who_doesnt_exist', status=500, expect_errors=True).status_code == 500
+
+@with_setup(login, teardown)
+def test_resource_protected_with_minimum_role_admin():
+    assert app.get('/page_for_admins').status == '200 OK'
+    # log out
+    assert app.get('/logout').status == REDIR
+    # drop the cookie
+    app.reset()
+    assert app.cookies == {}, "The cookie should be gone"
+    #login with user who has role 'user'
+    p = app.post('/login', {'username': 'user', 'password': "user"})
+    assert app.get('/page_for_admins').status == REDIR
 
 
 @with_setup(login, teardown)
