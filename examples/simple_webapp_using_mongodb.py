b8dd05e0a5d793c4a7f67fe85d7d0a123a83615e
Federico Ceratto <federico.ceratto@gmail.com>
2013-08-11 14:45:14 +0100

    HttpOnly cookies enable in the example applications.

--- examples/simple_webapp_using_mongodb.py
+++ examples/simple_webapp_using_mongodb.py
@@ -39,6 +39,7 @@ app = bottle.app()
 session_opts = {
     'session.cookie_expires': True,
     'session.encrypt_key': 'please use a random key and keep it secret!',
+    'session.httponly': True,
     'session.timeout': 3600 * 24,  # 1 day
     'session.type': 'cookie',
     'session.validate_key': True,
