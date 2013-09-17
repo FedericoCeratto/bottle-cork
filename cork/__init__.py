2f730727b324a80cffbfabe668016ac74aff9490
Federico Ceratto <federico.ceratto@gmail.com>
2013-09-17 17:58:52 +0100

    Bugfix: license set to LGPLv3.

--- cork/__init__.py
+++ cork/__init__.py
@@ -1,6 +1,6 @@
 # Cork - Authentication module for tyyhe Bottle web framework
 # Copyright (C) 2013 Federico Ceratto and others, see AUTHORS file.
-# Released under GPLv3+ license, see LICENSE.txt
+# Released under LGPLv3+ license, see LICENSE.txt
 #
 # Backends API - used to make backends available for importing
 #
