eba3c8bed40558c1e7cbb6fb319fe5034e117c28
Federico Ceratto <federico.ceratto@gmail.com>
2014-03-18 21:42:30 +0000

    Fix typo

--- tests/testutils.py
+++ tests/testutils.py
@@ -1,4 +1,4 @@
-# Cork - Authentication module for tyyhe Bottle web framework
+# Cork - Authentication module for the Bottle web framework
 # Copyright (C) 2013 Federico Ceratto and others, see AUTHORS file.
 # Released under LGPLv3+ license, see LICENSE.txt
 #
